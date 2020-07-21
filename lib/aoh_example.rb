ad9d4229af8d3b0b026775f6eacc878b7388769f
jade decordova <lacroixjaded@gmail.com>
2020-07-21 03:48:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,14 @@ def assembled_aoh
  ]
 end
 
-def literal_aoh
+def literal_aoh 
+  arrayofH = [
+    DON_G,
+   JOELLE_VD,
+   PAT_M,
+   KATE_G,
+   BRUCE_G
+  ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
